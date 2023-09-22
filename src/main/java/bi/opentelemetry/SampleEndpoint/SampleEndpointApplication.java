720cd3bb2d7af9fedd278b22ffda5ea08f71cb71
paula.bassaganas@boehringer-ingelheim.com <paula.bassaganas@boehringer-ingelheim.com>
2023-09-22 10:46:28 +0200

    opentelemetry initial config

--- src/main/java/bi/opentelemetry/SampleEndpoint/SampleEndpointApplication.java
+++ src/main/java/bi/opentelemetry/SampleEndpoint/SampleEndpointApplication.java
@@ -1,7 +1,11 @@
 package bi.opentelemetry.SampleEndpoint;
 
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class SampleEndpointApplication {
@@ -9,5 +13,8 @@ public class SampleEndpointApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SampleEndpointApplication.class, args);
 	}
-
+	@Bean
+	public OpenTelemetry openTelemetry() {
+		return AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
+	}
 }
